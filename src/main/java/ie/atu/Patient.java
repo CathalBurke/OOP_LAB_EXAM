50dcc70f35a699b00705c7072e49ae1063e3f537
CATHALBURKE-STUDENT <G00413483@atu.ie>
2025-04-07 11:19:19 +0100

    Added Fies And created Project For exam Setup Basic Constructors and Getters And Setters

---
+++ src/main/java/ie/atu/Patient.java
@@ -0,0 +1,48 @@
+package ie.atu;
+
+public class Patient {
+
+    public Patient(String given_name, String family_Name, String HSE_Number, int age) {
+        Given_name = given_name;
+        Family_Name = family_Name;
+        this.HSE_Number = HSE_Number;
+        Age = age;
+    }
+
+    public String getGiven_name() {
+        return Given_name;
+    }
+
+    public void setGiven_name(String given_name) {
+        Given_name = given_name;
+    }
+
+    public String getFamily_Name() {
+        return Family_Name;
+    }
+
+    public void setFamily_Name(String family_Name) {
+        Family_Name = family_Name;
+    }
+
+    public String getHSE_Number() {
+        return HSE_Number;
+    }
+
+    public void setHSE_Number(String HSE_Number) {
+        this.HSE_Number = HSE_Number;
+    }
+
+    public int getAge() {
+        return Age;
+    }
+
+    public void setAge(int age) {
+        Age = age;
+    }
+
+    String Given_name;
+    String Family_Name;
+    String HSE_Number ;
+    int Age;
+}
