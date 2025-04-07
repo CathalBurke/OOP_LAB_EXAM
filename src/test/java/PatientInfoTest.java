90569837a977ff1ab91294bda72e162e106f8d3a
CATHALBURKE-STUDENT <G00413483@atu.ie>
2025-04-07 12:17:11 +0100

    Added Sample tests to be tweaked to suit the Patient and ensure its within the ranges required

--- src/test/java/PatientInfoTest.java
+++ src/test/java/PatientInfoTest.java
@@ -1,2 +1,24 @@
+import ie.atu.Patient;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class PatientInfoTest {
+
+    private Patient Patient;
+
+    @BeforeEach
+    public void setup (){
+
+        Patient = new Patient();
+
+    }
+    @Test
+    public void Test_Given_name(){
+        Patient  = Patient.getGiven_name("John");
+        assert true("John");
+
+    }
+
+
+
 }
